24a02b6d5e17089864c88f3af0d2c888f2d8eb0d
marsmining <marsmining@gmail.com>
2014-08-11 08:29:46 +0200

    grabs a url, woot

--- Main.hs
+++ Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
 import Network.HTTP.Conduit
+import qualified Data.ByteString.Lazy as L
+
+forumUrl :: String
+forumUrl = "http://forums.sherdog.com/forums/f2/"
+
+get :: IO ()
+get = simpleHttp forumUrl >>= L.writeFile "foo.txt"
 
 main :: IO ()
-main = print "woot!"
+main = do
+     putStrLn "fetching.."
+     get
+     putStrLn "done"
+     ff
+
+ff :: IO ()
+ff = putStrLn "a" >> putStrLn "b"

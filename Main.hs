492ae9b4e8179a83aeb963e86bc54a7efa373683
marsmining <marsmining@gmail.com>
2014-08-31 11:44:13 +0200

    check tagsoup lib

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Network.HTTP.Conduit
 import qualified Data.ByteString.Lazy as L
+import qualified Text.HTML.TagSoup as S
 
 forumUrl :: String
 forumUrl = "http://forums.sherdog.com/forums/f2/"
@@ -18,3 +19,12 @@ main = do
 
 ff :: IO ()
 ff = putStrLn "a" >> putStrLn "b"
+
+html :: String
+html = "<html><body><div id='foo'>woot!</div></body></html>"
+
+gg :: [S.Tag String]
+gg = S.parseTags html
+
+hh :: [S.Tag String] -> [S.Tag String]
+hh = dropWhile (S.~/= "foo")
